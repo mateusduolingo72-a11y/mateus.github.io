a270c684085b2f5c35e3c7494e8f261e21fbea66
mateusduolingo72-a11y <mateus.duolingo72@gmail.com>
2026-04-01 00:25:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const stripe = new Stripe("SUA_SECRET_KEY_AQUI");
+const stripe = new Stripe("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");
 
 app.post("/create-checkout", async (req, res) => {
   const { amount } = req.body;
